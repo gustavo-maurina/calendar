d2260f4789cc5c52c95141d2d390f106ce43368e
Gustavo Farias <gustavocel82@gmail.com>
2022-05-31 20:39:24 -0300

    fix: typo

--- src/utils/getCalendarDays.js
+++ src/utils/getCalendarDays.js
@@ -3,7 +3,7 @@ import moment from "moment";
 import { CalendarDay } from "../components/CalendarDay/CalendarDay";
 
 export function getCalendarDays(date) {
-  const _date = moment(date).sclone();
+  const _date = moment(date).clone();
   const firstDay = _date.clone().startOf("month").startOf("week");
   const lastDay = _date.clone().endOf("month").endOf("week");
   const currentDay = firstDay.clone().subtract(1, "day");
